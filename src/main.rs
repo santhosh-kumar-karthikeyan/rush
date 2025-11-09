8e4db99d9d6a20d812b030729cd4d5a032ade4f8
Santhosh Kumar Karthikeyan <santhoshkumarkarthikeyan5@gmail.com>
2025-11-09 19:16:19 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    loop { 
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut command: String = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        let command: Vec<&str> = command.trim().split(" ").collect();
+        if command[0] == "exit" {
+            if command.len() > 1 {
+                let status: &i32 = &command[1].parse().unwrap();
+                process::exit(*status);
+            }
+            process::exit(0);
+        } else {
+            println!("{}: command not found", command[0]);
+        }
     }
 }
