b59cf705efc7049986f465e1f641a40ae8866574
Santhosh Kumar Karthikeyan <santhoshkumarkarthikeyan5@gmail.com>
2025-11-09 18:41:38 +0530

    [tes]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut command: String = String::new();
+    io::stdin()::read_line(&mut command).unwrap();
+    match command.trim() {
+        cmd => println!("{cmd}: command not found");
+    }
 }
